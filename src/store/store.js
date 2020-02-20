8b89423eba8dfbaca94a323e97961ae3b7361228
long20014 <hktngoc@onixdev2.com>
2020-02-20 17:20:06 +0700

    fix react router

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,15 @@ import rootReducer from 'reducers';
 
 const initialState = {};
 const middleware = [thunk];
+debugger
+const devTools = process.env.NODE_ENV === 'prod' ? null :
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(
   rootReducer, 
   initialState, 
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
