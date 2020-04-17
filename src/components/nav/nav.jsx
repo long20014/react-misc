e69f3760406efb43b89f896f3b12e02444b575dc
long20014 <long20014@yahoo.com>
2020-04-17 14:45:24 +0700

    fix router issue

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutAction } from 'actions/auth-action';
 
+const pURL = process.env.PUBLIC_URL;
 class Nav extends React.Component {
   constructor(props) {
     super(props);    
@@ -25,16 +26,16 @@ class Nav extends React.Component {
           <h3>Logo</h3>
           <ul className="nav-links">
             <li>
-              <Link to="/">Home</Link>
+              <Link to={pURL + "/"}>Home</Link>
             </li>
             <li>
-              <Link to="/clock">Clock</Link>
+              <Link to={pURL + "/clock"}>Clock</Link>
             </li>
             <li>
-              <Link to="/puzzle">Puzzle</Link>
+              <Link to={pURL + "/puzzle"}>Puzzle</Link>
             </li>
             <li>
-              <Link to="/card">Card</Link>
+              <Link to={pURL + "/card"}>Card</Link>
             </li>
           </ul>
           {isLoggedIn && <button onClick={this.logout} style={{ color: 'black' }}>Log out</button>}
